a229c0b5581b5fd61e88a6591a2ef296cc0faf40
Minakshi Kumari <jhaminakshi333@gmail.com>
2025-10-12 17:50:38 +0545

    Updated Featured section layout and styling

--- src/components/Home/Featured.jsx
+++ src/components/Home/Featured.jsx
@@ -10,8 +10,78 @@ const Featured = () => {
         {
             id:0,
             img:"/battery-auto.png",
+            name: "Apé NXT+ (Small Body)",
+            price: "4,50,000"
+        },
+        {
+            id:1,
+            img:"/battery-auto.png",
+            name: "Apé Auto Classic (Wide Body)",
+            price: "4,50,000"
+        },
+        {
+            id:2,
+            img:"/battery-auto.png",
+            name: "Apé Metro (Medium Body)",
+            price: "4,50,000"
+        },
+        {
+            id:3,
+            img:"/battery-auto.png",
+            name: "Classic Cargo Diesel 435cc",
+            price: "4,50,000"
+        },{
+            id:4,
+            img:"/battery-auto.png",
+            name: "Cargo CNG",
+            price: "4,50,000"
+        },{
+            id:5,
+            img:"/battery-auto.png",
+            name: "Cargo Diesel HD 599cc",
+            price: "4,50,000"
         }
-    ]
+    ];
+
+    const settings = {
+        dots : false,
+        infinite : true,
+        slidesToShow : 3,
+        SlidesToScroll: 1,
+        autoplay : true,
+        speed: 2000,
+        autoplaySpeed : 2000,
+        cssEase: "linear",
+        arrows: false,
+        responsive : [
+            {
+                brakpoint:1023,
+                settings:{
+                    dots : true,
+                    infinite : true,
+                    slidesToShow : 3,
+                    SlidesToScroll: 3,
+                },
+            },
+             {
+                brakpoint:768,
+                settings:{
+                   
+                    slidesToShow : 2,
+                    SlidesToScroll: 2,
+                    initialSlide: 2
+                },
+            },
+             {
+                brakpoint:480,
+                settings:{
+                   slidesToShow : 1,
+                    SlidesToScroll: 1,
+                    initialSlide: 2
+                },
+            },
+        ]
+    }
 
   return (
     <div>
